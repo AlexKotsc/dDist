39af2b1d4e3d45fc202d5ab0071c725c74a956be
DrGorgonzola <frej_korsgaard@hotmail.com>
2014-02-05 11:46:13 +0100

    Exercise 2 filer tilføjet

---
+++ Uge1/src/ex9/TextRemoveEvent.java
@@ -0,0 +1,14 @@
+package ex9;
+
+
+public class TextRemoveEvent extends MyTextEvent {
+
+	private int length;
+	
+	public TextRemoveEvent(int offset, int length) {
+		super(offset);
+		this.length = length;
+	}
+	
+	public int getLength() { return length; }
+}
